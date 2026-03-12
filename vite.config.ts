cae682c5dd91b809212f850cd715545f4b9c38a1
Evan Tragesser <etragess@purdue.edu>
2026-03-12 14:20:06 -0400

    Update to Vite 8 and other minor dependency updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const reactCompilerConfig = {}
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react({
-			babel: {
-				plugins: [['babel-plugin-react-compiler', reactCompilerConfig]]
-			}
+		react(),
+		babel({
+			presets: [reactCompilerPreset()]
 		})
 	],
+	build: {
+		rolldownOptions: {
+			experimental: {
+				lazyBarrel: true
+			}
+		}
+	},
 	base: '/marvel-remix-scoring'
 })
