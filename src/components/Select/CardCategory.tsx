68f58666cb58b985b8c48ebf2b0c1aca0419bc21
Evan Tragesser <etragess@purdue.edu>
2026-07-28 21:32:35 -0400

    Sort ignoring "the" at the start of a title

--- src/components/Select/CardCategory.tsx
+++ src/components/Select/CardCategory.tsx
@@ -11,8 +11,10 @@ interface SelectCardCategoryProps {
 }
 
 export const SelectCardCategory: FunctionComponent<SelectCardCategoryProps> = ({ cards, isExpanded, onClick }) => {
-	const { t } = useTranslation('card-info')
-	cards.sort((a, b) => t(`${a.id}.name`).localeCompare(t(`${b.id}.name`)))
+	const { t, i18n } = useTranslation('card-info')
+	const sortName = (card: Card) => t([`${card.id}.sortName`, `${card.id}.name`])
+	const collator = new Intl.Collator(i18n.language)
+	const sortedCards = cards.slice().sort((a, b) => collator.compare(sortName(a), sortName(b)))
 	const category = cards[0].type
 	const categoryName = CARD_TYPE[category].toLowerCase()
 	const nodeRef = useRef(null)
@@ -31,7 +33,7 @@ export const SelectCardCategory: FunctionComponent<SelectCardCategoryProps> = ({
 			<CSSTransition nodeRef={nodeRef} in={isExpanded} timeout={250} classNames="category-animated" unmountOnExit>
 				<div ref={nodeRef} className="select-card-category-wrapper-outer">
 					<div className="select-card-category-wrapper-inner">
-						{cards.map(c => (
+						{sortedCards.map(c => (
 							<SelectCard key={c.id} card={c} />
 						))}
 					</div>
